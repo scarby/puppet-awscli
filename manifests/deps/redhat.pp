ca6c8e06eee77d9e11dc4e551c6515169e3f1028
Adam Scarborough <scarby2@gmail.com>
2016-01-20 10:18:39 +0000

    the epel package puts /usr/bin/pip in path, puppet currently expects pip-python. Making a symlink until this is fixed

--- manifests/deps/redhat.pp
+++ manifests/deps/redhat.pp
@@ -9,11 +9,24 @@ class awscli::deps::redhat (
     include ::epel
     Package { require => Class['epel'] }
   }
+  #so the package from epel actually puts python-pip as pip command in path
+  #puppet expects python-pip
+
+  file{'/usr/bin/pip-python':
+    ensure => 'link',
+    target => '/usr/bin/pip',
+  }
 
   if ! defined(Package['python-devel']) {
-    package { 'python-devel': ensure => installed }
+    package { 'python-devel':
+      ensure  => installed,
+      require => File['/usr/bin/pip-python']
+    }
   }
   if ! defined(Package['python-pip']) {
-    package { 'python-pip': ensure => installed }
+    package { 'python-pip':
+      ensure  => installed,
+      require => File['/usr/bin/pip-python']
+    }
   }
 }
